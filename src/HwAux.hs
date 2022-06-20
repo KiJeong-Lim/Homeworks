0cc5ce1f617780efbfe530da55bc17e510262219
KiJeong-Lim <gijungdduk@naver.com>
2022-06-20 12:08:22 +0900

    .

--- src/HwAux.hs
+++ src/HwAux.hs
@@ -11,18 +11,18 @@ type ParserState = Int
 type ExitNumber = Int
 
 data Regex
-    = ReUnion Regex Regex
-    | ReConcat Regex Regex
-    | ReStar Regex
-    | ReChar MyChar
-    | ReEmpty
-    | ReNil
+    = ReUnion Regex Regex -- `ReUnions re1 re2` = `re1` ∪ `re2`
+    | ReConcat Regex Regex -- `ReConcat re1 re2` = `re1` `re2`
+    | ReStar Regex -- `ReStar re1` = `re1`*
+    | ReChar MyChar -- `ReChar ch` = 'ch'
+    | ReEmpty -- `ReEmpty` = ∅
+    | ReNil -- `ReNil` = ""
     deriving (Show)
 
 data NFA
     = NFA
-        { nfa_Q0 :: ParserState
-        , nfa_QF :: Map.Map ParserState ExitNumber
-        , nfa_delta :: Map.Map (ParserState, Maybe MyChar) (Set.Set ParserState)
+        { nfa_Q0 :: ParserState -- the initial state
+        , nfa_QF :: Map.Map ParserState ExitNumber -- final states
+        , nfa_delta :: Map.Map (ParserState, Maybe MyChar) (Set.Set ParserState) -- transitions
         }
     deriving (Show)
